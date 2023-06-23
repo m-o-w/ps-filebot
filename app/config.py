bda4568f4a7cf2ee08b3f60c03977570cb2ba19f
m-o-w <sting672744@gmail.com>
2023-06-23 16:42:06 +0530

    V13 Base

--- app/config.py
+++ app/config.py
@@ -1,1 +1,4 @@
 upload_directory = 'uploaded_files'
+container_connection_string = "DefaultEndpointsProtocol=https;AccountName=psfilebot;AccountKey=byiO1rvF/eO2nMmfi1IMVgt3fhZOQVaconPOT0sMkk78MY7aVzr7mXIAqDmx8NdppZJGengooZ9p+ASthzGEFw==;EndpointSuffix=core.windows.net"
+inbound_container = "inbound"
+index_container = "index"
