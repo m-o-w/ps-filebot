9dd70ecaf6de7f717a0c887ad3d3ebfd119b4b67
m-o-w <sting672744@gmail.com>
2023-06-30 15:20:19 +0530

    no azure function

--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ upload_directory = 'uploaded_files'
 container_connection_string = "DefaultEndpointsProtocol=https;AccountName=psfilebot;AccountKey=byiO1rvF/eO2nMmfi1IMVgt3fhZOQVaconPOT0sMkk78MY7aVzr7mXIAqDmx8NdppZJGengooZ9p+ASthzGEFw==;EndpointSuffix=core.windows.net"
 inbound_container = "inbound"
 index_container = "index"
+archive_container = "archive"
