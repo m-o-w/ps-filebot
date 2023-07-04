53eafc067476263a6f81bad5e1a94877d4853f65
m-o-w <sting672744@gmail.com>
2023-07-04 23:24:05 +0530

    stable v1

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import os
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 import streamlit as st
-from config import upload_directory, container_connection_string, inbound_container, index_container
+from config import upload_directory, container_connection_string, inbound_container, index_container, archive_container
 import random
 from azure.storage.blob import BlobServiceClient
 from llama_index import StorageContext, load_index_from_storage
@@ -12,6 +12,77 @@ import openai
 global_index = None
 iterator = random.randint(1, 99)
 print("Iterator: ",iterator)
+if os.environ.get("OPENAI_API_KEY") is not None:
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
+    
+
+def copy_index_to_blob(index_container, container_connection_string):
+    blob_service_client = BlobServiceClient.from_connection_string(container_connection_string)
+    # Get the source and destination container client
+    target_container_client = blob_service_client.get_container_client(index_container)
+    # Define the connection string and blob names
+    folder_path = index_container
+    for file_name in os.listdir(folder_path):
+        print(file_name)
+        blob_client = target_container_client.get_blob_client(index_container+"/"+file_name)
+        with open(os.path.join(folder_path, file_name), "rb") as data:
+            blob_client.upload_blob(data, overwrite=True)
+            print("Uploaded to Azure Blob storage as blob:\n\t" + file_name)
+    
+def archive_inbound_files():
+    # list all files in local inbound folder
+    inbound_files = os.listdir(inbound_container)
+    print(f"###############  Inbound files: {inbound_files}")
+    # archive each file
+    for filename in inbound_files:
+        archive_blob(filename, container_connection_string, inbound_container, archive_container)
+        
+     
+def archive_blob(filename, container_connection_string, source_container, archive_container):
+    # Define the connection string and blob names
+    blob_connection_string = container_connection_string
+    source_container_name = source_container
+    destination_container_name = archive_container
+    
+    print(f"###############  Archiving blob: {filename}")
+    print(f"###############  Connection string: {blob_connection_string}")
+    print(f"###############  Source container: {source_container_name}")
+    print(f"###############  Destination container: {destination_container_name}")
+    
+    # Move blob to archive container and delete source blob
+    blob_service_client = BlobServiceClient.from_connection_string(blob_connection_string)
+    # Get the source and destination container client
+    source_container_client = blob_service_client.get_container_client(source_container_name)
+    destination_container_client = blob_service_client.get_container_client(destination_container_name)
+    # Get the source blob client
+    source_blob_client = source_container_client.get_blob_client(filename)
+    destination_blob_client = destination_container_client.get_blob_client(filename)
+    destination_blob_client.start_copy_from_url(source_blob_client.url)
+    # Delete the source blob
+    source_blob_client.delete_blob()
+
+# Get all filers from blob storage 'inbound' container
+def get_files_from_blob_storage():
+    blob_service_client = BlobServiceClient.from_connection_string(container_connection_string)
+    container_client = blob_service_client.get_container_client(inbound_container)
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        # Get a blob client for each blob
+        blob_client = container_client.get_blob_client(blob.name)
+        # Download each blob to local folder
+        with open(os.path.join(inbound_container, blob.name), "wb") as file:
+        #with open(blob.name, "wb") as file:
+            file.write(blob_client.download_blob().readall())
+
+
+def merge_file_with_index(index, inbound_dir_name):
+    loader = SimpleDirectoryReader(inbound_dir_name, recursive=True, exclude_hidden=True)
+    new_documents = loader.load_data()
+    for docs in new_documents:
+        index.update_ref_doc(docs)
+    print(f"Documents count loaded: {len(new_documents)}")
+    print(f"###############  Index updated. Docs count in index: {index.docstore.docs}")
+    return index
 
 # Create method to load index from blob storage
 def load_index():
@@ -21,15 +92,24 @@ def load_index():
     container_client = blob_service_client.get_container_client(index_container)
     blob_list = container_client.list_blobs(prefix=index_container+"\\")
     for blob in blob_list:
+        print("Getting: ",blob.name)
         # Get a blob client for each blob
         blob_client = container_client.get_blob_client(blob.name)
         # Download each blob to local folder
         #with open(os.path.join(local_folder, blob.name), "wb") as file:
         with open(blob.name, "wb") as file:
-            file.write(blob_client.download_blob().readall())
+            file.write(blob_client.download_blob().readall())      
     
     storage_context = StorageContext.from_defaults(persist_dir="index")
     index = load_index_from_storage(storage_context)
+    
+    get_files_from_blob_storage()
+    index = merge_file_with_index(index, inbound_container)
+    index.storage_context.persist(persist_dir=index_container)
+    copy_index_to_blob(index_container, container_connection_string)
+    archive_inbound_files()
+    delete_files_in_directory(inbound_container)
+    
     st.session_state.index = index
     st.sidebar.success(f"Document Count: {len(index.docstore.docs)}")
     print("load_index from blob: ",iterator)
@@ -39,9 +119,12 @@ if "index" in st.session_state: # check if the index variable exists in the sess
     
 if not os.path.exists(upload_directory):
     os.makedirs(upload_directory)
+    
+if not os.path.exists(inbound_container):
+    os.makedirs(inbound_container)
 
-if not os.path.exists("index"):
-    os.makedirs("index")
+if not os.path.exists(index_container):
+    os.makedirs(index_container)
 
 def delete_files_in_directory(directory):
     for filename in os.listdir(directory):
@@ -51,7 +134,7 @@ def delete_files_in_directory(directory):
             print(f"Deleted file: {file_path}")
     
 # ------------------------------------Code for setting API Key-------------------------------------
-def button_click(user_api_key):
+def load_api_key(user_api_key):
     if len(user_api_key) < 30:
         st.sidebar.error("Please enter valid API Key")
         return
@@ -65,7 +148,7 @@ if os.environ.get("OPENAI_API_KEY") is None:
     st.sidebar.warning("Get your OpenAI API key from [here](https://platform.openai.com/account/api-keys).\n")
     button = st.sidebar.button("Load Key")
     if button:
-        button_click(user_api_key)
+        load_api_key(user_api_key)
 else:
     st.sidebar.success("API Key Loaded")
     reload_button = st.sidebar.button("Delete Key")
@@ -120,7 +203,9 @@ def answer_question(question):
     print("answer_question Called: ",iterator)
     query_engine=global_index.as_query_engine(openai_api_key=os.environ["OPENAI_API_KEY"])
     response = query_engine.query(question+"?")
+    st.info(question)
     st.info(response)
+    
 
 # File upload section
 uploaded_file = st.sidebar.file_uploader("Upload files to Blob")
