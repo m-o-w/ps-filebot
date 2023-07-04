ba66dd61825d7c0b1d8a4f66d3141e96a7714f8a
m-o-w <sting672744@gmail.com>
2023-07-04 18:35:15 +0530

    auth error check

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import os
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 import streamlit as st
-from config import upload_directory, container_connection_string, inbound_container, index_container, upload_directory, archive_container
+from config import upload_directory, container_connection_string, inbound_container, index_container
 import random
 from azure.storage.blob import BlobServiceClient
 from llama_index import StorageContext, load_index_from_storage
@@ -13,56 +13,26 @@ global_index = None
 iterator = random.randint(1, 99)
 print("Iterator: ",iterator)
 
-if not os.path.exists(inbound_container):
-    os.makedirs(inbound_container)
-if not os.path.exists(index_container):
-    os.makedirs(index_container)
-
-# Get files from inbound blob
-def get_files_from_blob():
-    print("get_files_from_blob Called: ",iterator)
-    delete_files_in_directory(inbound_container)
+# Create method to load index from blob storage
+def load_index():
+    print("load_index Called: ",iterator)
+    delete_files_in_directory(index_container)
     blob_service_client = BlobServiceClient.from_connection_string(container_connection_string)
-    container_client = blob_service_client.get_container_client(inbound_container)
-    blob_list = container_client.list_blobs(prefix=inbound_container+"\\")
+    container_client = blob_service_client.get_container_client(index_container)
+    blob_list = container_client.list_blobs(prefix=index_container+"\\")
     for blob in blob_list:
         # Get a blob client for each blob
         blob_client = container_client.get_blob_client(blob.name)
         # Download each blob to local folder
-        with open(os.path.join(inbound_container, blob.name), "wb") as file:
-        #with open(blob.name, "wb") as file:
+        #with open(os.path.join(local_folder, blob.name), "wb") as file:
+        with open(blob.name, "wb") as file:
             file.write(blob_client.download_blob().readall())
-
-# Create method to load index from blob storage
-def load_index():
-    print("load_index Called: ",iterator)
-    if os.path.exists(index_container+"/docstore.json"):
-        print("###############  Index found.")
-        storage_context = StorageContext.from_defaults(persist_dir="index")
-        index = load_index_from_storage(storage_context)
-        st.session_state.index = index
-        #st.sidebar.success(f"Document Count: {len(index.docstore.docs)}")
-        print("load_index from blob: ",iterator)
-        return index
-    else:
-        st.sidebar.error("Local index not found")
-
-def folder_contains_files(folder_path):
-    if os.path.exists(folder_path):
-        if os.path.isdir(folder_path):
-            if os.listdir(folder_path):
-                return True
-    return False
-def create_new_index(inbound_dir_name, index_dir_name):
-    print(f"###############  Creating new index: {index_dir_name}")
-    print(f"###############  Reading from DIR: {inbound_dir_name}")
-    loader = SimpleDirectoryReader(inbound_dir_name, recursive=True, exclude_hidden=True)
-    documents = loader.load_data()
-    print(f"Documents count loaded: {len(documents)}")
-    index = GPTVectorStoreIndex.from_documents(documents=documents)
-    index.storage_context.persist(persist_dir=index_dir_name)
-    print(f"###############  Index created. Docs count index: {index.docstore.docs}")      
-
+    
+    storage_context = StorageContext.from_defaults(persist_dir="index")
+    index = load_index_from_storage(storage_context)
+    st.session_state.index = index
+    st.sidebar.success(f"Document Count: {len(index.docstore.docs)}")
+    print("load_index from blob: ",iterator)
 
 if "index" in st.session_state: # check if the index variable exists in the session state object
     global_index = st.session_state.index # create a query engine from the index
@@ -130,9 +100,23 @@ def rebuild_index(from_directory):
     st.session_state.index = GPTVectorStoreIndex.from_documents(document)
     global_index = st.session_state.index.as_query_engine()
     
+# Read file dir and index files
+def index_files():
+    print("index_files Called: ",iterator)
+    # Check if no files uploaded
+    files = os.listdir(upload_directory)
+    if len(files) == 0:
+        st.sidebar.error("Please upload files")
+        return None
+        
+    # Create index and persist
+    if files is not None:
+        rebuild_index(upload_directory)
+        st.sidebar.write("Rebuild Index")
         
 # Generate response from index
 def answer_question(question):
+    openai.api_key = os.environ.get("OPENAI_API_KEY")
     print("answer_question Called: ",iterator)
     query_engine=global_index.as_query_engine(openai_api_key=os.environ["OPENAI_API_KEY"])
     response = query_engine.query(question+"?")
@@ -145,137 +129,18 @@ if uploaded_file:
     save_uploaded_file(uploaded_file)
     del uploaded_file
 
-def create_new_index(inbound_dir_name, index_dir_name):
-    print(f"###############  Creating new index: {index_dir_name}")
-    print(f"###############  Reading from DIR: {inbound_dir_name}")
-    loader = SimpleDirectoryReader(inbound_dir_name, recursive=True, exclude_hidden=True)
-    documents = loader.load_data()
-    print(f"Documents count loaded: {len(documents)}")
-    index = GPTVectorStoreIndex.from_documents(documents=documents)
-    index.storage_context.persist(persist_dir=index_dir_name)
-    print(f"###############  Index created. Docs count index: {index.docstore.docs}")
-
-def merge_file_with_index(index, inbound_dir_name):
-    loader = SimpleDirectoryReader(inbound_dir_name, recursive=True, exclude_hidden=True)
-    new_documents = loader.load_data()
-    for docs in new_documents:
-        index.update_ref_doc(docs)
-    print(f"Documents count loaded: {len(new_documents)}")
-    print(f"###############  Index updated. Docs count in index: {index.docstore.docs}")
-    return index
-
-def create_new_index(inbound_dir_name, index_dir_name):
-    print(f"###############  Creating new index: {index_dir_name}")
-    print(f"###############  Reading from DIR: {inbound_dir_name}")
-    loader = SimpleDirectoryReader(inbound_dir_name, recursive=True, exclude_hidden=True)
-    documents = loader.load_data()
-    print(f"Documents count loaded: {len(documents)}")
-    index = GPTVectorStoreIndex.from_documents(documents=documents)
-    index.storage_context.persist(persist_dir=index_dir_name)
-    print(f"###############  Index created. Docs count index: {index.docstore.docs}")   
-
-def archive_blob(container_connection_string, source_container, archive_container):
-    # Define the connection string and blob names
-    blob_connection_string = container_connection_string
-    source_container_name = source_container
-    destination_container_name = archive_container
-    
-    print(f"###############  Connection string: {blob_connection_string}")
-    print(f"###############  Source container: {source_container_name}")
-    print(f"###############  Destination container: {destination_container_name}")
-    
-    # Move blob to archive container and delete source blob
-    blob_service_client = BlobServiceClient.from_connection_string(blob_connection_string)
-    # Get the source and destination container client
-    source_container_client = blob_service_client.get_container_client(source_container_name)
-    destination_container_client = blob_service_client.get_container_client(destination_container_name)
-    # Get the source blob client
-    blob_list = source_container_client.list_blobs()
-    #blob_list = container_client.list_blobs()
-    for blob in blob_list:
-        filename = blob.name
-        print(f"###############  Archiving blob: {filename}")
-        source_blob_client = source_container_client.get_blob_client(filename)
-        destination_blob_client = destination_container_client.get_blob_client(filename)
-        destination_blob_client.start_copy_from_url(source_blob_client.url)        
-        # Delete the source blob
-        source_blob_client.delete_blob()
-
-def copy_index_to_blob(index_dir_name, container_connection_string):
-    blob_service_client = BlobServiceClient.from_connection_string(container_connection_string)
-    # Get the source and destination container client
-    target_container_client = blob_service_client.get_container_client(index_container) 
-    # Define the connection string and blob names
-    folder_path = index_dir_name
-    for file_name in os.listdir(folder_path):
-        print(file_name)
-        blob_client = target_container_client.get_blob_client(index_container+"/"+file_name)
-        with open(os.path.join(folder_path, file_name), "rb") as data:
-                blob_client.upload_blob(data, overwrite=True)
-                print("Uploaded to Azure Blob storage as blob:\n\t" + file_name) 
-            
-# Read file dir and index files
-def get_index_from_container():
-    # read files from blob storage container 'inbound'
-    print("get_index_from_container Called: ",iterator)
-    print("index_container: ",index_container)
-    delete_files_in_directory(index_container)
-    blob_service_client = BlobServiceClient.from_connection_string(container_connection_string)
-    container_client = blob_service_client.get_container_client(index_container)
-    blob_list = container_client.list_blobs(prefix=index_container+"\\")
-    #blob_list = container_client.list_blobs()
-    for blob in blob_list:
-        print("Blob found: ",blob.name)
-        # Get a blob client for each blob
-        blob_client = container_client.get_blob_client(blob.name)
-        # Download each blob to local folder
-        with open(blob.name, "wb") as file:
-            print("Writing : ",file)
-            file.write(blob_client.download_blob().readall())
-
-def reporcess_index():
-    get_index_from_container()
-    get_files_from_blob()
-    inbound_contains_files = folder_contains_files(inbound_container)
-    if inbound_contains_files:
-        index_contains_files = folder_contains_files(index_container)
-        if index_contains_files:
-            index = load_index()
-            new_index = merge_file_with_index(index, inbound_container)
-            new_index.storage_context.persist(persist_dir=index_container)
-            print(f"###############  Index created. Docs count index: {new_index.docstore.docs}")
-            copy_index_to_blob(index_container, container_connection_string)
-            archive_blob(container_connection_string, inbound_container, archive_container)
-            st.sidebar.success(f"Document Count: {len(index.docstore.docs)}")
-            delete_files_in_directory(inbound_container)
-            return
-        else:
-            create_new_index(inbound_container, index_container)
-            index = load_index()
-            copy_index_to_blob(index_container, container_connection_string)
-            archive_blob(container_connection_string, inbound_container, archive_container)
-            st.sidebar.success(f"Document Count: {len(index.docstore.docs)}")
-            delete_files_in_directory(inbound_container)
-    else:
-        load_index()
-        if "index" in st.session_state:
-            index = st.session_state.index
-            st.sidebar.success(f"Document Count: {len(index.docstore.docs)}")
-        else: st.sidebar.error("Please upload files")
-
 # Create reload button on sidebar
 reload_button = st.sidebar.button("Reload Index")
 if reload_button:
-    reporcess_index()
+    load_index()
         
 def main():
     print("main Called: ",iterator)
             
     #Start StreamLit
-    st.title("PS File AI Bot (V13) Mod")
+    st.title("PS File AI Bot (V13)")
     form = st.form("Input box", clear_on_submit=True)
     question = form.text_input("Send message", placeholder="Your question here")
-
     submit_button = form.form_submit_button("Send")
     
     # Someone have asked a question
@@ -295,8 +160,7 @@ def main():
             return
         if "index" in st.session_state: # check if the index variable exists in the session state object
             answer_question(question) 
-
-
+        
 if __name__ == "__main__":
     print("__name__ Called: ",iterator)
     main()
