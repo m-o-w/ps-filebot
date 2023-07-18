8effdbec51328cc1033ce9c3a2da3e8789a59e38
m-o-w <sting672744@gmail.com>
2023-07-18 20:17:10 +0530

    Fix 3

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ from azure.storage.blob import BlobServiceClient
 from llama_index import StorageContext, load_index_from_storage
 import openai
 
-st.set_page_config(page_title="PS File Hunter")
+st.set_page_config(page_title="DocumentGenius")
 # Load index from session if found
 global_index = None
 iterator = random.randint(1, 99)
@@ -235,9 +235,9 @@ def main():
     print("main Called: ",iterator)
             
     #Start StreamLit
-    st.title("PS File AI Bot (V13)")
+    st.title("DocumentGenius")
     form = st.form("Input box", clear_on_submit=True)
-    question = form.text_input("Send message", placeholder="Your question here")
+    question = form.text_input("Query", placeholder="Your question here")
     submit_button = form.form_submit_button("Send")
     
     # Someone have asked a question
